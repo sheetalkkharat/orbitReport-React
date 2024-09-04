c766e44caff13850b84cd0b3257d4118c89a6411
sheetalkkharat <sheetalkharatk@gmail.com>
2024-09-03 20:20:58 -0500

    GA-6

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,15 +2,24 @@ const Table = ({ sat }) => {
   return (
     <table>
     <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
+    <tr>
+        <th>Name</th>
+        <th>Type of Satellite</th>
+        <th>Launch Date</th>
+        <th>Status</th>
+    </tr>
+    </thead>
+        {sat.map((data, id) => {
+            return (
+        <tbody>
+            <tr>
+                <td key={id}>{data.name}</td>
+                <td key={id}>{data.type}</td>
+                <td key={id}>{data.launchDate}</td>
+                <td key={id}>{data.operational}</td>
+            </tr>
+        </tbody>
+            )})};
    </table>
   );
 };
